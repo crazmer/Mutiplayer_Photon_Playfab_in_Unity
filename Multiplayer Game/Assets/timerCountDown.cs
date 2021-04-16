d9b503949a4e64edc06c71f3cb406fe5c3588206
ChinmayKawale <70374433+ChinmayKawale@users.noreply.github.com>
2021-04-16 20:53:21 +0530

    CountDown Timer Updated
    Timer set according to minutes and seconds, can be changed or set through inspector

---
+++ Multiplayer Game/Assets/timerCountDown.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class timerCountDown : MonoBehaviour
+{
+    [SerializeField] float startTime = 5f;
+    [SerializeField] Slider slider;
+    [SerializeField] Text timerText;
+
+    float timer = 0f;
+    void Start()
+    {
+        StartCoroutine(Timer());
+    }
+
+    private IEnumerator Timer()
+    {
+         timer = startTime;
+
+        do
+        {
+            timer -= Time.deltaTime;
+            slider.value = timer / startTime;
+            FormatText();
+            yield return null;
+        }
+        while (timer>0);
+    }
+    private void FormatText()
+    {
+        int minutes = (int)(timer / 60) % 60 ;
+        int seconds = (int)(timer % 60);
+
+        timerText.text = "";
+
+        if(minutes>0)
+        {
+            timerText.text += minutes + "m";
+        }
+        if (seconds > 0)
+        {
+            timerText.text += seconds + "s";
+        }
+    }
+    
+}
