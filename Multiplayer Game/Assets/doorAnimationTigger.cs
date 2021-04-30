ad7a01e3b60804a7fac4f1ea18d14cc5ba1e2d36
Atharva Mali <atharvamali.135@gmail.com>
2021-04-30 15:27:25 +0530

    AutoDoorOpen

---
+++ Multiplayer Game/Assets/doorAnimationTigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorAnimationTigger : MonoBehaviour
+{
+    public Animator animator;
+    void Start()
+    {
+        //animator = GetComponent<Animator>();
+        animator.SetBool("open", false);
+        animator.SetBool("Close", false);
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        animator.SetBool("open", true);
+        animator.SetBool("Close", false);
+    }
+    public void OnTriggerExit(Collider other)
+    {
+        animator.SetBool("Close", true);
+        animator.SetBool("open", false);
+    }
+}
