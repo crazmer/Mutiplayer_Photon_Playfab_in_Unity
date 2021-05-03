59c2b234855123121db8da980c6dedee4f52e9bb
ChinmayKawale <70374433+ChinmayKawale@users.noreply.github.com>
2021-05-03 22:19:07 +0530

    AI Scene Added
    Main Player to be added and shooting to be implemented

---
+++ Multiplayer Game/Assets/Scripts/AI/Enemy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Enemy : MonoBehaviour
+
+{
+
+	public float speed = 10.0f;
+	public float accuracy = 0.01f;
+	public Transform goal;
+
+
+	void LateUpdate()
+	{
+		this.transform.LookAt(goal.position);
+		Vector3 direction = goal.position - this.transform.position; //This is the vector between the goal position and the Character.
+		//Debug.DrawRay(this.transform.position, direction, Color.red);
+
+		if (direction.magnitude > accuracy)
+		{
+			this.transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
+		}
+
+	}
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Bullet")
+        {
+			Destroy(gameObject);
+        }
+    }
+}
---
+++ Multiplayer Game/Assets/Scripts/AI/WayPointController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WayPointController : MonoBehaviour
+{
+    public List<Transform> waypoints = new List<Transform>();
+    private Transform targetWaypoint;
+    private int targetWaypointIndex=0;
+    private float minDistance = 0.1f;
+    private float lastWaypointIndex;
+
+    public float movementSpeed = 1f;
+    private float roatationSpedd = 2f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastWaypointIndex = waypoints.Count - 1;
+        targetWaypoint = waypoints[targetWaypointIndex]; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float movemntStep = movementSpeed * Time.deltaTime;
+        float rotationStep = roatationSpedd * Time.deltaTime;
+
+        Vector3 directionToTarget = targetWaypoint.position - transform.position;
+        Quaternion rotationToTarget = Quaternion.LookRotation(directionToTarget);
+
+        Debug.DrawRay(transform.position, transform.forward * 50f,Color.green,0f);
+        Debug.DrawRay(transform.position, directionToTarget, Color.red, 0f);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotationToTarget, rotationStep);
+
+        float distance = Vector3.Distance(transform.position, targetWaypoint.position);
+        CheckDistanceToWayPoint(distance);
+        //Debug.Log(distance);
+
+        transform.position = Vector3.MoveTowards(transform.position, targetWaypoint.position,movementSpeed);
+
+    }
+    void CheckDistanceToWayPoint(float currentDistance)
+    {
+        if (currentDistance<=minDistance)
+        {
+            targetWaypointIndex++;
+            UpdateTargetWaypoint();
+        }
+    }
+    void UpdateTargetWaypoint()
+    {
+        if(targetWaypointIndex > lastWaypointIndex)
+        {
+            targetWaypointIndex = 0;
+        }
+        targetWaypoint = waypoints[targetWaypointIndex];
+    }
+}
